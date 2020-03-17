abb2d46d219154ee7cbdd7f8532c2943f8e5af13
ongtao <carson.t.wang@gmail.com>
2020-03-17 17:26:26 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -6,9 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 # Timezone
-sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
-sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
+sed -i "s/timezone='UTC'/timezone='CST-8'/" openwrt/package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./openwrt/package/base-files/files/bin/config_generate
 # firewall custom
-echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> openwrt/package/network/config/firewall/files/firewall.user
